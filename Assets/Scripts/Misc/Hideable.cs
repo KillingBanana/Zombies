37b73577ac91a4697e6c7b8f4a5cefa4278d6bc8
KBanana <killingbanana@gmail.com>
2017-12-01 22:03:59 +0100

    .

--- Assets/Scripts/Misc/Hideable.cs
+++ Assets/Scripts/Misc/Hideable.cs
@@ -1,45 +1,24 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.Rendering;
+﻿using UnityEngine;
 
 public class Hideable : MonoBehaviour {
-	private const float transparency = 0.1f;
+	[SerializeField] private Material opaqueMaterial;
+	[SerializeField] private Material transparentMaterial;
+	[SerializeField] private Hideable[] hideablesToHide;
+	private new Renderer renderer;
+	public bool hidden;
+	private bool transparent;
 
-	private Material material;
-	private Hideable[] hideablesInChildren;
-	private Hideable[] hideablesInParents;
-	[HideInInspector] public bool hidden;
-	[HideInInspector] private bool transparent;
-
-	private void Start() {
-		material = GetComponent<MeshRenderer>().material;
+	private void Awake() {
+		renderer = GetComponent<Renderer>();
 	}
 
 	private void Update() {
-
-		hideablesInChildren = GetComponentsInChildren<Hideable>();
-		hideablesInParents = GetComponentsInParent<Hideable>();
-		
-		Hideable[] hideablesToHide = hideablesInChildren.Concat(hideablesInParents).ToArray();
-		
 		if (hidden && !transparent) {
-			if (hideablesToHide.Length > 0) {
-				foreach (Hideable hideable in hideablesToHide) {
-					hideable.hidden = true;
-					hideable.MakeMaterialTransparent();
-				}
-			}
-			MakeMaterialTransparent();
+			MakeTransparent();
 		}
 
 		if (!hidden && transparent) {
-			if (hideablesToHide.Length > 0) {
-				foreach (Hideable hideable in hideablesToHide) {
-					hideable.hidden = false;
-				}
-			}
-			MakeMaterialOpaque();
+			MakeOpaque();
 		}
 	}
 
@@ -55,34 +34,23 @@ public class Hideable : MonoBehaviour {
 		}
 	}
 
-	private void MakeMaterialTransparent() {
-		material.SetFloat("_Mode", hidden ? 3 : 0);
-		material.SetInt("_SrcBlend", (int) BlendMode.One);
-		material.SetInt("_DstBlend", (int) BlendMode.OneMinusSrcAlpha);
-		material.SetInt("_ZWrite", 0);
-		material.DisableKeyword("_ALPHATEST_ON");
-		material.DisableKeyword("_ALPHABLEND_ON");
-		material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-		material.renderQueue = 3000;
-
-		Color color = material.color;
-		color.a = transparency;
-		material.color = color;
-		transparent = true;
+	protected void MakeOpaque() {
+		if (transparent) {
+			renderer.material = opaqueMaterial;
+			transparent = false;
+		}
+		foreach (Hideable hideable in hideablesToHide) {
+			hideable.MakeOpaque();
+		}
 	}
 
-	private void MakeMaterialOpaque() {
-		material.SetInt("_SrcBlend", (int) BlendMode.One);
-		material.SetInt("_DstBlend", (int) BlendMode.Zero);
-		material.SetInt("_ZWrite", 1);
-		material.DisableKeyword("_ALPHATEST_ON");
-		material.DisableKeyword("_ALPHABLEND_ON");
-		material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-		material.renderQueue = -1;
-
-		Color color = material.color;
-		color.a = 1;
-		material.color = color;
-		transparent = false;
+	protected void MakeTransparent() {
+		if (!transparent) {
+			renderer.material = transparentMaterial;
+			transparent = true;
+		}
+		foreach (Hideable hideable in hideablesToHide) {
+			hideable.MakeTransparent();
+		}
 	}
 }
